3d4c839d525f47ac4fff5a6cb6b4eae9da3c55d8
H-ozomgbachi <ozomgbachih@gmail.com>
2021-12-24 19:41:52 +0100

    Add project files.

---
+++ BaseSystem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace BaseSystem
+{
+    public class BaseSystem
+    {
+        public static string DecimalToNonHexaBase(int decimalNumber, int newBase)
+        {
+            int dividend = decimalNumber;
+            List<string> remainders = new();
+            while (dividend > 0)
+            {
+                int remainder = dividend % newBase;
+                remainders.Add(remainder.ToString());
+                dividend = (dividend - remainder) / newBase;
+            }
+            remainders.Reverse();
+            return string.Join(string.Empty, remainders);
+        }
+    }
+}
